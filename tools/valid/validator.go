22e7f9cd4e9d0c6c94cff9f050645a865eaa7387
Alejandro Mery <amery@jpi.io>
2022-02-02 19:30:09 +0000

    tools/valid: add `path` and `pathdis` custom tag validators
    Signed-off-by: Alejandro Mery <amery@jpi.io>

--- tools/valid/validator.go
+++ tools/valid/validator.go
@@ -14,3 +14,21 @@ var (
 func IsPath(str string) bool {
 	return rxPath.MatchString(str)
 }
+
+// register cusom validator tags
+func init() {
+	valid.TagMap["path"] = valid.Validator(func(str string) bool {
+		if str == "" {
+			return true
+		}
+
+		return rxPath.MatchString(str)
+	})
+	valid.TagMap["pathdis"] = valid.Validator(func(str string) bool {
+		if str == "" || str == "-" {
+			return true
+		}
+
+		return rxPath.MatchString(str)
+	})
+}
